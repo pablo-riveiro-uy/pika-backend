c0b632e4feb3f5aae5a06a40894f3a57abd8032c
pablo-riveiro-uy <priveiro@gmail.com>
2026-01-06 11:00:19 -0300

    adding landing

--- api/urls.py
+++ api/urls.py
@@ -3,12 +3,14 @@ from rest_framework.routers import DefaultRouter
 from django.conf import settings
 from django.views.static import serve
 from .views import EventViewSet, PhotoViewSet, event_photos_slide, event_photo_upload, event_photos_json, event_manager, event_manager_select
+from django.views.generic import TemplateView 
 
 router = DefaultRouter()
 router.register(r'events', EventViewSet, basename='event')
 router.register(r'photos', PhotoViewSet, basename='photo')
 
 urlpatterns = [
+    path("", TemplateView.as_view(template_name="landing/landing.html"), name="landing"),
     path('', include(router.urls)),
     path("event/<uuid:token>/slide/", event_photos_slide, name="event_photos_slide"),
     path("event/<uuid:token>/upload/", event_photo_upload, name="event_photo_upload"),
