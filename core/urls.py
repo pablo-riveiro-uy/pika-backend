4aa7f575d88a1c177697933377a48b0ef07bf258
pablo-riveiro-uy <priveiro@gmail.com>
2026-01-06 11:10:24 -0300

    fixing routs

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,18 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.static import serve
-
+from django.views.generic import TemplateView
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
 )
 
 urlpatterns = [
+     path(
+        "",
+        TemplateView.as_view(template_name="landing/landing.html"),
+        name="landing",
+    ),
     path("admin/", admin.site.urls),
     path("api/", include("api.urls")),
 
